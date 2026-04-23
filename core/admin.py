b1435850a199b1007d22c1f1da871439be3257c7
Chiemerie <emchi.cf@gmail.com>
2026-04-23 13:18:32 +0100

    Add all Foundational models and register in admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import FarmUnit, Pen, Worker, PenWorkerAssignment
+from .models import FarmUnit, Pen, Worker, PenWorkerAssignment, Flock, Supplier, FlockPlacement, FeedType, DrugAndSupplement
 
 # Register your models here.
 admin.site.register(FarmUnit)
 admin.site.register(Pen)
 admin.site.register(Worker)
 admin.site.register(PenWorkerAssignment)
+admin.site.register(Flock)
+admin.site.register(Supplier)
+admin.site.register(FlockPlacement)
+admin.site.register(FeedType)
+admin.site.register(DrugAndSupplement)
