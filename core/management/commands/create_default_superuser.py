52138e1e5067198d84b2e5b568b7067b2c6f0b2e
Chiemerie <emchi.cf@gmail.com>
2026-05-02 14:17:01 +0100

    Read admin password from environment variable

--- core/management/commands/create_default_superuser.py
+++ core/management/commands/create_default_superuser.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
@@ -6,13 +7,12 @@ class Command(BaseCommand):
     help = 'Creates or fixes the default superuser'
 
     def handle(self, *args, **kwargs):
+        password = os.environ.get('ADMIN_PASSWORD', 'farmadmin2026')
         user, created = User.objects.get_or_create(username='admin')
-        user.set_password('farmadmin2026')
+        user.set_password(password)
         user.is_staff = True
         user.is_superuser = True
         user.is_active = True
+        user.email = 'admin@farm.com'
         user.save()
-        if created:
-            self.stdout.write('Superuser created')
-        else:
-            self.stdout.write('Superuser fixed')
+        self.stdout.write(f'Admin user {"created" if created else "updated"} with password from environment')
