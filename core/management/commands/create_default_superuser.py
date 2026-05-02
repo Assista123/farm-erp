46db4db6f0353cd6d5bcccaae5ecdb172fee8fd9
Chiemerie <emchi.cf@gmail.com>
2026-05-02 13:37:55 +0100

    Fix superuser creation to ensure staff and superuser flags are set

--- core/management/commands/create_default_superuser.py
+++ core/management/commands/create_default_superuser.py
@@ -3,18 +3,16 @@ from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
-    help = 'Creates or updates the default superuser'
+    help = 'Creates or fixes the default superuser'
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='admin@farm.com',
-                password='farmadmin2026'
-            )
-            self.stdout.write('Superuser created successfully')
+        user, created = User.objects.get_or_create(username='admin')
+        user.set_password('farmadmin2026')
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save()
+        if created:
+            self.stdout.write('Superuser created')
         else:
-            user = User.objects.get(username='admin')
-            user.set_password('farmadmin2026')
-            user.save()
-            self.stdout.write('Superuser password updated')
+            self.stdout.write('Superuser fixed')
