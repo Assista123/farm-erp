ddc8a72f940975769808e308d695df8721e1f62c
Chiemerie <emchi.cf@gmail.com>
2026-05-02 12:08:54 +0100

    Update superuser command to reset password if user exists

--- core/management/commands/create_default_superuser.py
+++ core/management/commands/create_default_superuser.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
-    help = 'Creates a default superuser if none exists'
+    help = 'Creates or updates the default superuser'
 
     def handle(self, *args, **kwargs):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
                 username='admin',
                 email='admin@farm.com',
-                password='changeme123'
+                password='farmadmin2026'
             )
             self.stdout.write('Superuser created successfully')
         else:
-            self.stdout.write('Superuser already exists')
+            user = User.objects.get(username='admin')
+            user.set_password('farmadmin2026')
+            user.save()
+            self.stdout.write('Superuser password updated')
